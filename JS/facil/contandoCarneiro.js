5802d1bc95e6c2f1d5a677de4bf966688f44d6c0
MateusdeNunes <mateusonunes95@gmail.com>
2023-01-24 16:57:35 -0300

    exercicio finalizado

--- JS/facil/contandoCarneiro.js
+++ JS/facil/contandoCarneiro.js
@@ -1,7 +1,7 @@
-Dado //uma variavel x inteira positiva (maior que zero), 3 por exemplo, imprime na tela a seguinte fala: 1 carneirinho...2 carneirinho...3 carneirinho...
+//uma variavel x inteira positiva (maior que zero), 3 por exemplo, imprime na tela a seguinte fala: 1 carneirinho...2 carneirinho...3 carneirinho...
 
 const solucao = x =>{
-    for (let i=0; i<= x; i++){
+    for (let i=1; i<= x; i++){
     console.log(`${i} carneirinho...`);
         
     }
