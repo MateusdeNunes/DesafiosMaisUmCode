ff7a081807146904c31979ccf083403c71720f24
MateusdeNunes <mateusonunes95@gmail.com>
2023-03-06 10:26:43 -0300

    desafio do dia 27/02

--- JS/facil/comparandostring.js
+++ JS/facil/comparandostring.js
@@ -8,12 +8,9 @@
 
 //Caracteres em uma string são indexados da esquerda para a direita.
 //O índice do primeiro caractere é 0, e o índice do último caractere de uma string chamada stringName é stringName.length - 1.
-const terminaIgual = (palavra, fimDaPalavra)=>{
-   if (palavra.indexOf(palavra.lenght)=fimDaPalavra.indexOf(palavra.lenght-fimDaPalavra)){
-   return true;
-   }else{
-    return false;
-   }
-}
-console.log(terminaIgual("cafe", 'fe')); //true
-console.log(terminaIgual("banana", 'fe'));
+function stringEndsWith(str, ending) {
+    return str.endsWith(ending);
+  }
+  const string = 'cafe';
+const ending = 'fe';
+console.log(stringEndsWith(string, ending)); // Output: true
